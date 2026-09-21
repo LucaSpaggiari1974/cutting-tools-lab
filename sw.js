87359dce28059c5d6f5a097ae301477457e8bb18
LucaSpaggiari1974 <spaggiari@me.com>
2026-09-21 16:12:41 +0200

    Refresh PWA cache for manufacturer photo sync

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="cutting-tools-lab-v16";
+const CACHE="cutting-tools-lab-v17";
 const APP=["./","./index.html","./manifest.webmanifest","./icon.svg","./catalog.json","./manufacturers.json"];
 
 self.addEventListener("install",event=>{
