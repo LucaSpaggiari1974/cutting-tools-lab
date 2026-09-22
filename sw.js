b86ba6580d3ed0f7b47aa49f8a3b0f87868c4a16
LucaSpaggiari1974 <spaggiari@me.com>
2026-09-22 06:06:31 +0200

    Forza aggiornamento cache PWA Windows alla versione v19

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="cutting-tools-lab-v18";
+const CACHE="cutting-tools-lab-v19";
 const APP=["./","./index.html","./manifest.webmanifest","./icon.svg","./catalog.json","./manufacturers.json"];
 
 self.addEventListener("install",event=>{
