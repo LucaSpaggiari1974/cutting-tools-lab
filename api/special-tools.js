99d77abdcac794d008eeb638619fd555cdbba8c7
LucaSpaggiari1974 <spaggiari@me.com>
2026-09-22 09:26:38 +0200

    Abilita autenticazione password da browser

--- api/special-tools.js
+++ api/special-tools.js
@@ -107,7 +107,7 @@ function setCors(res) {
   res.setHeader("Access-Control-Allow-Origin", "https://lucaspaggiari1974.github.io");
   res.setHeader("Vary", "Origin");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-special-tools-password");
 }
 
 
