4668ed9e862ff017dd37adfebb97bf63cde23249
LucaSpaggiari1974 <spaggiari@me.com>
2026-09-22 06:54:13 +0200

    Bump service worker cache for photo preview fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="cutting-tools-lab-v24";
+const CACHE="cutting-tools-lab-v25";
 const APP=["./","./index.html","./manifest.webmanifest","./icon.svg","./catalog.json","./manufacturers.json"];
 
 self.addEventListener("install",event=>{
