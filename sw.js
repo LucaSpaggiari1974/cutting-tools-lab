1ea63468b4477f4cbc8b40d46fc7b11b0fdeff92
LucaSpaggiari1974 <spaggiari@me.com>
2026-09-23 05:19:10 +0200

    Refresh Windows PWA cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="cutting-tools-lab-v41";
+const CACHE="cutting-tools-lab-v42";
 const APP=["./","./index.html","./manifest.webmanifest","./icon.svg","./catalog.json","./manufacturers.json"];
 self.addEventListener("install",event=>{event.waitUntil((async()=>{const c=await caches.open(CACHE);await c.addAll(APP);await self.skipWaiting();})());});
 self.addEventListener("activate",event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim();})());});
